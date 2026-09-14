3a7a4720d02317a12dba4fa39efacc9594a67abe
vigneshwarang47 <vigneshwaran.g47@gmail.com>
2026-09-14 20:45:53 +0530

    unit test cases upated

---
+++ tests/test_operation.py
@@ -0,0 +1,12 @@
+from src.math_operation import add,sub
+
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(-1,1) == 0
+    assert add(5,2) == 7
+
+def test_sub():
+    assert sub(5,3)==2
+    assert sub(4,3) == 1
+    assert sub(3-3) == 0
